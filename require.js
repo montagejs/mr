88cf4c7e7313e3149a0742bb5dbc1aacfb4f8b18
Kris Kowal <kris.kowal@cixar.com>
2013-08-27 09:45:32 -0700

    Add support for compilers

--- require.js
+++ require.js
@@ -32,6 +32,7 @@ Require.makeRequire = function (config) {
     config.compile = config.compile || config.makeCompiler(config);
     config.parseDependencies = config.parseDependencies || Require.parseDependencies;
     config.read = config.read || Require.read;
+    config.compilers = config.compilers || {};
 
     // Modules: { exports, id, location, directory, factory, dependencies,
     // dependees, text, type }
@@ -67,7 +68,7 @@ Require.makeRequire = function (config) {
     }
 
     // Ensures a module definition is loaded, compiled, analyzed
-    var load = memoize(function (topId, viaId) {
+    var load = memoize(function (topId, viaId, loading) {
         var module = getModuleDescriptor(topId);
         return Promise.try(function () {
             // if not already loaded, already instantiated, or
@@ -82,15 +83,23 @@ Require.makeRequire = function (config) {
         })
         .then(function () {
             // compile and analyze dependencies
-            config.compile(module);
-            var dependencies =
-                module.dependencies =
-                    module.dependencies || [];
-            if (module.redirect !== void 0) {
-                dependencies.push(module.redirect);
-            }
-            if (module.extraDependencies !== void 0) {
-                Array.prototype.push.apply(module.dependencies, module.extraDependencies);
+            var extension = Require.extension(topId);
+            if (config.compilers[extension]) {
+                var compilerId = config.compilers[extension];
+                return deepLoad(compilerId, "", loading)
+                .then(function () {
+                    var compile = require(compilerId);
+                    compile(module);
+                });
+            } else {
+                config.compile(module);
+                var dependencies = module.dependencies = module.dependencies || [];
+                if (module.redirect !== void 0) {
+                    dependencies.push(module.redirect);
+                }
+                if (module.extraDependencies !== void 0) {
+                    Array.prototype.push.apply(module.dependencies, module.extraDependencies);
+                }
             }
         });
     });
@@ -111,6 +120,7 @@ Require.makeRequire = function (config) {
         .then(function () {
             // load the transitive dependencies using the magic of
             // recursion.
+            var dependencies = module.dependencies = module.dependencies || [];
             return Promise.all(module.dependencies.map(function (depId) {
                 depId = resolve(depId, topId);
                 // create dependees set, purely for debug purposes
@@ -530,6 +540,7 @@ function configurePackage(location, description, parent) {
     config.location = location || Require.getLocation();
     config.packageDescription = description;
     config.useScriptInjection = description.useScriptInjection;
+    config.compilers = description.compilers;
 
     if (description.production !== void 0) {
         config.production = description.production;
@@ -691,6 +702,13 @@ Require.base = function (location) {
     return String(location)
         .replace(/(.+?)\/+$/, "$1")
         .match(/([^\/]+$|^\/$|^$)/)[1];
+};
+
+Require.extension = function (location) {
+    var match = /\.([^\/]+)$/.exec(location);
+    if (match) {
+        return match[1];
+    }
 };
 
 // Tests whether the location or URL is a absolute.
