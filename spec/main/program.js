a81293279078d815882cf7b4706f9b3d3a491c11
Stuart Knightley <stuart@stuartk.com>
2013-09-10 17:39:35 -0700

    Remove rogue debugger statement

--- spec/main/program.js
+++ spec/main/program.js
@@ -1,6 +1,5 @@
 var test = require('test');
 
-debugger;
 test.assert(require('dot-slash') === 10, 'main with "./"');
 test.assert(require('js-ext') === 20, 'main with ".js" extension');
 test.assert(require('no-ext') === 30, 'main with no extension');
