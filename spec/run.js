5a03400a7bc6c15962c6872295ba183f9490128f
Stuart Knightley <stuart@stuartk.com>
2013-09-13 17:45:17 -0700

    Add Jasmine JS reporter

--- spec/run.js
+++ spec/run.js
@@ -5,10 +5,14 @@ var jasmineEnv = jasmine.getEnv();
 jasmineEnv.updateInterval = 1000;
 
 var htmlReporter = new jasmine.HtmlReporter();
+// for phantom.js
 this.jsApiReporter = new jasmine.JsApiReporter();
+// for Saucelabs
+var jsReporter = new jasmine.JSReporter();
 
 jasmineEnv.addReporter(htmlReporter);
 jasmineEnv.addReporter(this.jsApiReporter);
+jasmineEnv.addReporter(jsReporter);
 
 jasmineEnv.specFilter = function(spec) {
     return htmlReporter.specFilter(spec);
