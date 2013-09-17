09a44bf05333716cb1e4830984ec8d0abab38777
Kris Kowal <kris.kowal@cixar.com>
2013-09-16 22:34:46 -0700

    Fix overlays + compilers and redirects
    These features previously did not compose.  Now, compilers and redirects
    are captured from the configuration after overlays have been applied.

--- boot/preload-boilerplate.js
+++ boot/preload-boilerplate.js
@@ -1092,8 +1092,6 @@ function configurePackage(location, description, parent) {
     config.location = location || Require.getLocation();
     config.packageDescription = description;
     config.useScriptInjection = description.useScriptInjection;
-    config.compilers = description.compilers;
-    config.translators = description.translators;
 
     if (description.production !== void 0) {
         config.production = description.production;
@@ -1213,6 +1211,10 @@ function configurePackage(location, description, parent) {
         );
     });
     config.mappings = mappings;
+
+    // compilers, translators, redirect patterns
+    config.compilers = description.compilers;
+    config.translators = description.translators;
 
     return config;
 }
