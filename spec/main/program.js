83eb0bad2ef25101decc0d47df5600844a631a44
Stuart Knightley <stuart@stuartk.com>
2013-11-13 11:29:44 -0800

    Add test for requiring a package's main module without .js extension

--- spec/main/program.js
+++ spec/main/program.js
@@ -4,4 +4,6 @@ test.assert(require('dot-slash') === 10, 'main with "./"');
 test.assert(require('js-ext') === 20, 'main with ".js" extension');
 test.assert(require('no-ext') === 30, 'main with no extension');
 
+test.assert(require('js-ext') === require("js-ext/a"), 'can require "main" without extension');
+
 test.print('DONE', 'info');
