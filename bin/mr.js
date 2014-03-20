f62c1bd5ae500e605f07de071328e9a47552b451
Kris Kowal <kris.kowal@cixar.com>
2014-03-19 17:25:18 -0700

    Move bin specific code into bin/mr

--- bin/mr.js
+++ bin/mr.js
@@ -1,2 +1,30 @@
 #!/usr/bin/env node
-require("../node").boot().done();
+
+var Require = require("../require");
+var Optimist = require("optimist");
+var URL = require("url");
+var Q = require("q");
+var FS = require("q-io/fs");
+
+var argv = Optimist.argv;
+var program = argv._.shift();
+
+Require.findPackageLocationAndModuleId(program)
+.then(function (info) {
+    return Require.loadPackage(info.location, {})
+    .then(function (pkg) {
+        return pkg.async(info.id);
+    });
+}, function (error) {
+    var location = Require.filePathToLocation(program);
+    var directory = URL.resolve(location, "./");
+    var file = FS.relativeFromDirectory(directory, location);
+    var descriptions = {};
+    descriptions[directory] = Q({});
+    return Require.loadPackage(directory, {
+        descriptions: descriptions
+    }).then(function (pkg) {
+        return pkg.async(file);
+    });
+})
+.done();
