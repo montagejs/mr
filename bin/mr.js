f9034f0e39877ef07c26066f279f7dd956b29f87
Kris Kowal <kris.kowal@cixar.com>
2014-03-19 17:25:18 -0700

    Move bin specific code into bin/mr

--- bin/mr.js
+++ bin/mr.js
@@ -1,2 +1,29 @@
-#!/usr/bin/env node --harmony_weakmaps --harmony_proxies
-require("../node").boot().done();
+#!/usr/bin/env node --harmony
+
+var Require = require("../require");
+var Optimist = require("optimist");
+var URL = require("url");
+var Q = require("q");
+var FS = require("q-io/fs");
+
+var argv = Optimist.argv;
+var program = argv._.shift();
+
+Require.findPackageLocationAndModuleId(program)
+.then(function (info) {
+    return Require.loadPackage(info.location, {
+    })
+    .invoke("async", info.id);
+}, function (error) {
+    var location = Require.filePathToLocation(program);
+    var directory = URL.resolve(location, "./");
+    var file = FS.relativeFromDirectory(directory, location);
+    var descriptions = {};
+    descriptions[directory] = Q({});
+    return Require.loadPackage(directory, {
+        descriptions: descriptions
+    })
+    .invoke("async", file);
+})
+.done();
+
