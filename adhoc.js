eed028a356b2c8b3ac2ab9bef1fa7105738e8366
Harold Thetiot <hthetiot@gmail.com>
2017-07-17 17:22:12 -0700

    fix adhoc.js

--- adhoc.js
+++ adhoc.js
@@ -1,15 +1,32 @@
 /* global URL:true */
 
 var URL = require("mini-url");
-var QS = require("qs");
 
-var a = document.createElement("a");
+var TRIM_REG = /\+/g;
+function decodeHash(s) { 
+    return decodeURIComponent(s.replace(TRIM_REG, " ")); 
+}
+
+var PARSE_PARAM_REG = /([^&;=]+)=?([^&;]*)/g;
+function getQueryParams() {
+
+    var matches,
+        hashParams = {},
+        hashValue = location.search.toString().substr(1);
+
+    while ((matches = PARSE_PARAM_REG.exec(hashValue))) {
+       hashParams[decodeHash(matches[1])] = decodeHash(matches[2]);
+    }
+
+    return hashParams;
+}
+
 
 var packageLocation;
 var moduleId;
 
 if (window.location.search) {
-    var query = QS.parse(window.location.search.slice(1));
+    var query = getQueryParams();
     var packageLocation = query['package-location'];
     var moduleId = query['module-id'];
     document.querySelector("[name=package-location]").value = packageLocation;
@@ -23,7 +40,9 @@ function run(packageLocation, moduleId) {
 
     console.log("Require:", "package:", JSON.stringify(packageLocation), "id:", JSON.stringify(moduleId));
     require.loadPackage(packageLocation)
-    .invoke("async", moduleId)
+    .then(function (pkg) {
+        return pkg.async(moduleId);
+    })
     .then(function (exports) {
         console.log("Exports:", exports);
         console.log("Packages:", require.packages);
