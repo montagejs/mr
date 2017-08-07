41e00ec18e9db2b4b774103138da7771b5cdd462
Harold Thetiot <hthetiot@gmail.com>
2017-08-07 09:33:41 -0700

    update module-resolution spec

--- test/spec/module-resolution/program.js
+++ test/spec/module-resolution/program.js
@@ -1,8 +1,8 @@
 
 var test = require('test');
 
-var WeakMap = require('weak-map').WeakMap;
-test.assert(WeakMap === 1, 'should load weak-map from node_module');
+var WeakMap = require('sub-module').WeakMap;
+test.assert(WeakMap === 2, 'should load ./weak-map.js from node_module');
 
-var MyWeakMap = require('./weak-map').WeakMap;
-test.assert(MyWeakMap === 2, 'should load ./weak-map.js');
+var WeakMap = require('sub-module/weak-map').WeakMap;
+test.assert(WeakMap === 1, 'should load weak-map from node_module');
--- test/spec/module-resolution/weak-map.js
+++
@@ -1,1 +0,0 @@
-exports.WeakMap = 2;
