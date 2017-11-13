c4d1a2c8dd7807187d8a78680610bffe22be9478
Thibault Zanini <thibaultzanini@gmail.com>
2017-11-13 10:41:13 -0500

    Allow to require a deserialized object-descriptor and improvements. Deserialized object-descriptor can be directly required on the module’s property name `montageObject`. Rename delegate method requireWillCompileMJSONFILE to compileMJSONFile.

--- require.js
+++ require.js
@@ -1141,11 +1141,19 @@
     Require.MetaCompiler = function (module) {
         if (module.location && (endsWith(module.location, ".meta") || endsWith(module.location, ".mjson"))) {
             if (typeof module.exports !== "object" && typeof module.text === "string") {
-                if (Require.delegate && typeof Require.delegate.requireWillCompileMJSONFile === "function") {
-                    return Require.delegate.requireWillCompileMJSONFile(
+                if (Require.delegate && typeof Require.delegate.compileMJSONFile === "function") {
+                    return Require.delegate.compileMJSONFile(
                         module.text, module.require, module.id
                     ).then(function (root) {
-                        module.exports = root || JSON.parse(module.text);
+                        module.exports = JSON.parse(module.text);
+                        if (module.exports.montageObject) {
+                            throw new Error(
+                                'using reserved word as property name, \'montageObject\' at: ' +
+                                module.location
+                            );
+                        }
+
+                        module.exports.montageObject = root;
                         return module;
                     });
                 } else {
