4a209315a5fa85bb7404ecf5395a6f2a1641807f
Thomas Jaede <thomas.jaede@kaazing.com>
2018-05-15 09:49:17 -0700

    Remove start-of-string designation on comment regExps

--- test/spec/comments/program.js
+++ test/spec/comments/program.js
@@ -29,6 +29,8 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 </copyright> */
 // require("a");
+ // require("a");
 /* require("b"); */
+ /* require("b"); */
 var test = require('test');
 test.print('DONE', 'info');
